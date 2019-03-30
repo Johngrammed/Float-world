0b52db9980860dbabf3d7039dbf7af8de204d073
John <48957779+Johngrammed@users.noreply.github.com>
2019-03-30 04:02:41 +0300

    Облачка
    теперь летают

---
+++ Float world/Assets/scripts/Cloud1.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool RightToLeft;
+    void Start()
+    {
+        if (Random.Range(0, 2) == 1)
+        {
+             transform.position = new Vector3(-11f, Random.Range(-3f, 4f), 10f);
+             RightToLeft = true;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+        else
+        {
+             transform.position = new Vector3(11f, Random.Range(-3f, 4f), 10f);
+             RightToLeft = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > 11f  || transform.position.x < -11f)
+        {
+            Destroy(gameObject);
+        }
+
+        if (RightToLeft)
+        {
+            transform.position = new Vector3(GetComponent<Transform>().position.x + .01f, GetComponent<Transform>().position.y, GetComponent<Transform>().position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(GetComponent<Transform>().position.x - .01f, GetComponent<Transform>().position.y, GetComponent<Transform>().position.z);
+        }
+    }
+}
---
+++ Float world/Assets/scripts/CloudCreator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCreator : MonoBehaviour
+{
+    public GameObject Cloud;
+    public bool CloudOnScreen = false;
+
+    private float timeInSecondsP;
+    private int minutesP;
+    private int secondsP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeInSecondsP += Time.deltaTime;
+
+        secondsP = (int)(timeInSecondsP % 60);
+        if (secondsP >= 40)
+        {
+            Instantiate(Cloud);
+            timeInSecondsP = Random.Range(0.0f, 10.0f);
+        }
+    }
+}
