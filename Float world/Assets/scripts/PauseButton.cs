f9b6c19f6fcdf3d3471a039b450caca38be981d1
John <48957779+Johngrammed@users.noreply.github.com>
2019-04-14 17:12:38 +0300

    menu animation

---
+++ Float world/Assets/scripts/PauseButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    
+    public GameObject PauseMenu;
+    //public GameObject ShadowMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pause()
+    {
+
+        
+        PauseMenu.GetComponent<Animator>().SetBool("Pause", true);
+        //ShadowMenu.GetComponent<Animator>().SetBool("Active", true);
+    }
+
+}
